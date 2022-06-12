d823755805743ce039f9eb31dbf7289241a414bb
zhushaolong <zhushaolong@xender.com>
2022-06-12 19:22:18 +0800

    init

---
+++ src/main/java/cn/dd/staticblog/StaticblogApplication.java
@@ -0,0 +1,13 @@
+package cn.dd.staticblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaticblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StaticblogApplication.class, args);
+    }
+
+}
